deb04a935fb53b4bb9cdbc3161f44d1ff8d438b1
dhanushka365 <pasindudhanushka365@gmail.com>
2024-08-01 22:32:37 +0530

    Imported DecimalPipe and RouterLink

--- src/app/layout/top-bar/top-bar.component.ts
+++ src/app/layout/top-bar/top-bar.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { DecimalPipe } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-top-bar',
+  standalone:true,
+  imports: [DecimalPipe , RouterLink],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.css'
 })
